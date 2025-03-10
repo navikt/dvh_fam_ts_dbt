423fdd6e791562c65bb0ca4bbb302061477e5ec8
nav-gard <gard.sigurd.troim.henriksen@nav.no>
2025-03-10 13:09:34 +0100

    Fikset feil i utpakking av liste fra nested path

--- models/TS_utpakking_v2/ts_meldinger_til_aa_pakke_ut_v2.sql
+++ models/TS_utpakking_v2/ts_meldinger_til_aa_pakke_ut_v2.sql
@@ -5,22 +5,34 @@
 }}
 
 with ts_meta_data as (
-  SELECT distinct m.pk_ts_meta_data
-    FROM {{ source ('fam_ef', 'fam_ts_meta_data_v2') }} m,
-        JSON_TABLE(
-            m.melding,
-            '$'
-            COLUMNS(
-                type VARCHAR2(100) PATH '$.vedtaksperioder.lovverketsMålgruppe',
-                vedtak_resultat VARCHAR2(100) PATH '$.vedtak_resultat'
-            )
-        ) j
-    WHERE (j.type = 'ENSLIG_FORSØRGER'
-    OR j.vedtak_resultat = 'AVSLÅTT')
-    AND m.endret_tid > nvl( (select max(endret_tid) from {{ source ('fam_ef', 'fam_ts_fagsak_v2') }}), m.endret_tid-1 )
+  SELECT DISTINCT 
+    m.pk_ts_meta_data
+  FROM 
+    {{ source('fam_ef', 'fam_ts_meta_data_v2') }} m,
+    JSON_TABLE(
+      m.melding,
+      '$'
+      COLUMNS (
+        vedtak_resultat VARCHAR2(100) PATH '$.vedtak_resultat',
+        NESTED PATH '$.vedtaksperioder[*]'
+        COLUMNS (
+          lovverkets_maalgruppe VARCHAR2(100) PATH '$.lovverketsMålgruppe'
+        )
+      )
+    ) j
+  WHERE 
+    (j.lovverkets_maalgruppe = 'ENSLIG_FORSØRGER' OR j.vedtak_resultat = 'AVSLÅTT')
+    AND m.endret_tid > NVL(
+      (SELECT MAX(endret_tid) FROM {{ source('fam_ef', 'fam_ts_fagsak_v2') }}), 
+      m.endret_tid - 1
+    )
 )
 
-select meta.*
-from {{ source ('fam_ef', 'fam_ts_meta_data_v2') }} meta
-join ts_meta_data
-on meta.pk_ts_meta_data = ts_meta_data.pk_ts_meta_data
+SELECT 
+  meta.*
+FROM 
+  {{ source('fam_ef', 'fam_ts_meta_data_v2') }} meta
+JOIN 
+  ts_meta_data
+ON 
+  meta.pk_ts_meta_data = ts_meta_data.pk_ts_meta_data
