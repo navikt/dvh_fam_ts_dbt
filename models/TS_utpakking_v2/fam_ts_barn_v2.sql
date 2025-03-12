265e4caa91d08ef47c8d8c2bc756f5309543244b
nav-gard <gard.sigurd.troim.henriksen@nav.no>
2025-03-12 14:53:35 +0100

    Fikset problem med fam_ts_barn_v2 som forhindret lesing av fnr, la til constraint på vedtaksperiode

--- models/TS_utpakking_v2/fam_ts_barn_v2.sql
+++ models/TS_utpakking_v2/fam_ts_barn_v2.sql
@@ -10,23 +10,25 @@ with ts_meta_data as (
   select * from {{ref ('ts_meldinger_til_aa_pakke_ut_v2')}}
 ),
 
-ts_fagsak as (
-  select * from {{ref ('fam_ts_fagsak_v2')}}
+ts_vedtaksperioder as (
+  select * from {{ref ('fam_ts_vedtaksperioder_v2')}}
 ),
 
 pre_final as (
 select * from ts_meta_data,
   json_table(melding, '$'
     COLUMNS (
-      nested        path '$.vedtaksperioder.barn.barn[*]' columns (
-        fnr         varchar2 path '$.fnr'
-      )
+        nested path '$.vedtaksperioder[*]' columns (fra_og_med DATE path '$.fom', til_og_med DATE path '$.tom',
+        nested path '$.barn.barn[*]' columns (fnr varchar2 path '$.fnr')
+        )
     )
   ) j
 ),
 
 til_fk_person1 as (
     select
+    fra_og_med,
+    til_og_med,
     ekstern_behandling_id,
     fnr,
     nvl(ident.fk_person1, -1) fk_person1
@@ -42,15 +44,17 @@ final as (
     t.fnr,
     t.fk_person1,
     t.ekstern_behandling_id,
-    b.pk_ts_FAGSAK as FK_ts_FAGSAK
+    v.pk_ts_vedtaksperioder as FK_ts_vedtaksperioder
   from til_fk_person1 t
-  join ts_fagsak b
-  on t.ekstern_behandling_id = b.ekstern_behandling_id
+  join ts_vedtaksperioder v
+  on t.ekstern_behandling_id = v.ekstern_behandling_id
+  and t.fra_og_med = v.fra_og_med
+  and t.til_og_med = v.til_og_med
 )
 
 select
   dvh_famef_kafka.hibernate_sequence.nextval as PK_ts_barn,
-  FK_ts_FAGSAK,
+  FK_TS_VEDTAKSPERIODER,
   case wheN fk_person1 = -1  THEN fnr
       ELSE NULL
     END fnr,
